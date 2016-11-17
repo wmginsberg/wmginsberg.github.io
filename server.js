4b2fe575008803515e013958841af0972baf13e5
Wendy Ginsberg <wmginsberg@gmail.com>
2016-11-17 10:06:37 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+// Node.js notation for importing packages
+var express = require('express');
+
+// Spin up a server
+var app = express();
+
+
+// Serve static files from the main directory
+app.use(express.static(__dirname));
+var port = process.env.PORT || 8083;
+
+//  ____________________________________
+// |                                    |
+// |       Setting up Page Routes       |
+// |____________________________________|
+//
+
+app.get('/', function(req, res){
+    res.sendFile("index.html", {root: '.'});
+});
+
+app.get('/blog', function(req, res){
+    res.sendFile("index.html", {root: '.'});
+});
+
+app.get('/blog/poly-express', function(req, res){
+    res.sendFile("blog/poly-express.html", {root: '.'});
+});
+
+// Tell the app to listen for requests on port 3001
+app.listen(port, function () {
+  console.log('Example app listening on port '+ port + '!');
+});
